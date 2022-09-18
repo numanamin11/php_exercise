ff692b226031fb1636d1e5c49ec6170cf6e8b703
numanamin11 <30683455+numanamin11@users.noreply.github.com>
2022-09-18 14:05:10 +0400

    Notifications Queueable

--- app/Notifications/SendEmail.php
+++ app/Notifications/SendEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class SendEmail extends Notification
+class SendEmail extends Notification implements ShouldQueue
 {
     use Queueable;
 
